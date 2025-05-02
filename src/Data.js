bf297378150ff67b6c0d132580178370cae3c485
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-02 19:08:14 +0600

    added new component

--- src/Data.js
+++
@@ -1,8 +0,0 @@
-export const getPosts = async () =>{
-   const response = await fetch("https://jsonplaceholder.typicode.com/users", {
-
-     method:"get",
-   })
-   return await response.json()
-
-}
--- src/components/Random-jokes/Jokes.jsx
+++ src/components/Random-jokes/Jokes.jsx
@@ -16,7 +16,7 @@ const Jokes = () => {
         <div className='w-full h-screen flex justify-center items-center bg-[#23272F]'>
             <div className='sm:w-[450px] w-full sm:mx-0 mx-5 h-auto sm:h-[250px] bg-green-800 hover:shadow-slate-500 hover:shadow-md transition ease-in-out duration-300 text-white  rounded-lg flex flex-col justify-between items-center p-5'>
                 <h1 className='text-2xl mb-3 sm:mb-0 sm:text-4xl'>Random Jokes Hub</h1>
-                <p className='text-gray-300 text-center font-semibold transition-all duration-500 my-3 sm:my-0'>{jokes.value}</p>
+                <p className='text-gray-300 text-center font-semibold transition-all duration-500 my-3 sm:my-0'> {jokes.value}</p>
                 <button onClick={getData} className='py-1 px-5 rounded-md bg-[#23272F] text-[#58C4DC] cursor-pointer'>Generate Jokes</button>
             </div>
         </div>
