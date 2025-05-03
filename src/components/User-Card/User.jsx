35a52e04aaa6b76ca32c373a3a565a052501d02f
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-03 12:36:28 +0600

    added

--- src/components/User-Card/User.jsx
+++ src/components/User-Card/User.jsx
@@ -13,7 +13,8 @@ const User = () => {
             <div>
                 <h1 className='text-2xl text-center font-bold'>User Card</h1>
                 <div className='w-[300px] h-[250px] rounded-lg  bg-gray-400 shadow-lg flex flex-col justify-center items-center mt-5'>
-                    <h2 className='text-2xl text-blue-600'> user id</h2>
+                    <img src="" alt="" />
+                    <h1 className='text-xl font-bold'>Name</h1>
 
                 </div>
             </div>
