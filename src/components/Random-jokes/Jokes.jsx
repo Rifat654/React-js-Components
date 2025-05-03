73f416145c57c9f53b0963c5be37d6db3d0a1401
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-03 12:44:10 +0600

    add new css

--- src/components/Random-jokes/Jokes.jsx
+++ src/components/Random-jokes/Jokes.jsx
@@ -14,9 +14,9 @@ const Jokes = () => {
 
     return (
         <div className='flex flex-col justify-center items-center mt-10'>
-            <div className='sm:w-[450px] w-full sm:mx-0 mx-5 h-auto sm:h-[250px] bg-green-800 hover:shadow-slate-500 hover:shadow-md transition ease-in-out duration-300 text-white  rounded-lg flex flex-col justify-between items-center p-5'>
+            <div className='sm:w-[450px] w-full sm:mx-0 mx-5 h-auto  bg-green-800 hover:shadow-slate-500 hover:shadow-md transition ease-in-out duration-300 text-white  rounded-lg flex flex-col justify-between items-center p-5'>
                 <h1 className='text-2xl mb-3 sm:mb-0 sm:text-4xl'>Random Jokes Hub</h1>
-                <p className='text-gray-300 text-center font-semibold transition-all duration-500 my-3 sm:my-0'> {jokes.value}</p>
+                <p className='text-gray-300 text-center font-semibold transition-all duration-500 my-3 sm:my-10'> {jokes.value}</p>
                 <button onClick={getData} className='py-1 px-5 rounded-md bg-[#23272F] text-[#58C4DC] cursor-pointer'>Generate Jokes</button>
             </div>
         </div>
