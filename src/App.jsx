9ad25ef9431179f9570cba88b02c27b9d7e82001
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-07 00:01:38 +0600

    added new section

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import API from './components/Slider/API'
 import Jokes from './components/Random-jokes/Jokes'
 import User from './components/User-Card/User'
+import Acordain from './components/FAQ/Acordain'
 
 
 const App = () => {
   return (
-    <div className=' bg-]'>
+    <div >
       <Jokes />
       <User />
+      <Acordain />
       {/* <API /> */}
     </div>
   )
