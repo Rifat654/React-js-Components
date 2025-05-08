4eb5d8f8839d4967ca4189c86af7999f5f2ba6ff
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-08 13:03:12 +0600

    add

--- src/components/FAQ/Accordain.jsx
+++ src/components/FAQ/Accordain.jsx
@@ -1,9 +1,11 @@
-// import React, { useState } from 'react'
+import React, { useState } from 'react'
 import { data } from '../../API/Data.js'
 const Accordain = () => {
+    const [open, setOpen] = useState(false)
+    const Click = () => {
+        setOpen(!open)
+    }
 
-
-    // const [open, setOpen] = useState(null)
     return (
         <div>
             <div className=' text-white h-[400px] text-center mt-20 '>
@@ -11,12 +13,19 @@ const Accordain = () => {
 
                 <div className='flex flex-col items-center justify-center px-5 mt-10'>
                     {
-                        data.map((item) => (
-                            <div className='w-full mx-5 sm:mx-0 sm:w-[550px]  bg-purple-900 flex items-center justify-between p-4 rounded-lg mb-4' key={item.id}>
-                                <p key={item.id} >{item.question}</p>
-                                <span className='cursor-pointer text-2xl font-bold'>+</span>
-                            </div>
-                        ))
+                        data.map((item, index) => {
+                            return (
+                                <div key={index} className='w-full max-w-[600px] bg-[#1F2937] rounded-lg shadow-md mb-4 p-4  flex flex-col items-center justify-between' >
+                                    <div className='flex items-center justify-between w-full pb-5' >
+                                        <h3>{item.question}</h3>
+                                        <button onClick={Click} className='text-2xl font-bold cursor-pointer'>{open ? <span>-</span> : <span>+</span>}</button>
+                                    </div>
+                                    <p> {open ? item.answer : ""}</p>
+                                </div>
+
+                            )
+
+                        })
                     }
                 </div>
             </div>
