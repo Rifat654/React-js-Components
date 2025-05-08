87ad3f591d1030e7fdfaa0c15b9b1b38139bb47f
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-08 12:03:30 +0600

    added

--- src/components/FAQ/Accordain.jsx
+++ src/components/FAQ/Accordain.jsx
@@ -9,10 +9,10 @@ const Accordain = () => {
             <div className=' text-white h-[400px] text-center mt-20 '>
                 <h1 className='text-3xl font-bold'>FAQ</h1>
 
-                <div className='flex flex-col items-center justify-center mt-10'>
+                <div className='flex flex-col items-center justify-center px-5 mt-10'>
                     {
                         data.map((item) => (
-                            <div className='w-[550px] bg-purple-900 flex items-center justify-between p-4 rounded-lg mb-4' key={item.id}>
+                            <div className='w-full mx-5 sm:mx-0 sm:w-[550px]  bg-purple-900 flex items-center justify-between p-4 rounded-lg mb-4' key={item.id}>
                                 <p key={item.id} >{item.question}</p>
                                 <span className='cursor-pointer text-2xl font-bold'>+</span>
                             </div>
