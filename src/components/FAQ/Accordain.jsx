48858350a18cfdae847831d012a0b5a397ecdf0c
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-09 14:36:47 +0600

    added

--- src/components/FAQ/Accordain.jsx
+++ src/components/FAQ/Accordain.jsx
@@ -8,7 +8,7 @@ const Accordain = () => {
 
     return (
         <div>
-            <div className=' text-white h-[400px] text-center mt-20 '>
+            <div className=' text-gray-400 h-[400px] text-center mt-20 '>
                 <h1 className='text-3xl font-bold'>FAQ</h1>
 
                 <div className='flex flex-col items-center justify-center px-5 mt-10'>
@@ -18,7 +18,7 @@ const Accordain = () => {
                                 <div key={index} className='w-full max-w-[600px] bg-[#1F2937] rounded-lg shadow-md mb-4 p-4  flex flex-col items-center justify-between' >
                                     <div className='flex items-center justify-between w-full pb-5' >
                                         <h3>{item.question}</h3>
-                                        <button onClick={Click} className='text-2xl font-bold cursor-pointer'>{open ? <span>-</span> : <span>+</span>}</button>
+                                        <button onClick={Click} className=' text-2xl font-bold cursor-pointer'>{open ? <span>-</span> : <span>+</span>}</button>
                                     </div>
                                     <p> {open ? item.answer : ""}</p>
                                 </div>
