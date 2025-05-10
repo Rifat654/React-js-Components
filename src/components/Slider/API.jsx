c310cda1cf9a045cfb4eec421d38c004155ee589
Rifat654 <141209933+Rifat654@users.noreply.github.com>
2025-05-10 12:56:43 +0600

    added

--- src/components/Slider/API.jsx
+++ src/components/Slider/API.jsx
@@ -1,9 +1,27 @@
-
+import { useEffect, useState } from "react"
 
 const API = () => {
+
+    const [user, setUser] = useState([])
+    useEffect(() => {
+        const Userdata = async () => {
+            const response = await fetch("https://fakestoreapi.com/products");
+            const data = await response.json();
+            console.log(data);
+            setUser(data)
+
+        }
+        Userdata();
+    })
     return (
         <div className="text-gray-200 h-[400px] text-center my-20 ">
-            <h1>Hello world</h1>
+            <div>
+                {
+                    user.map((list) => (
+                        <p>{list.price}</p>
+                    ))
+                }
+            </div>
         </div>
     )
 }
